373da46338a5ad602af4d4d57ff27d26384aa566
Thomas Seignette <tseignette@enseirb-matmeca.fr>
2019-08-19 20:37:27 +0200

    Fix for build + TODO list added

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Gallery
 
-TODO: doc
+Gallery, a macOS Photos alternative
+
+
+## TODO
+
+* update thumbnails path so that it doesn't generate them again if root folder is moved
+* add a progress indicator at the bottom of the aside section
+* implement settings save (remember gallery folder)
+* implement different view types
+* image zoom feature
+* write a nice presentation page
+* implement folder creation and edition
+* favorite feature
+* advanced selection feature
+* trash feature instead of instant deletion
+* doc
+* tests
--- src/app/@theme/components/media/media.component.ts
+++ src/app/@theme/components/media/media.component.ts
@@ -8,31 +8,47 @@ import { Image, Video } from '../../../@core/models';
 })
 export class MediaComponent implements OnChanges {
 
-  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('video', { static: false }) templateVideo: ElementRef;
 
   @Input() media: Image|Video;
 
   duration: string;
+
+  image: Image;
+
+  video: Video;
 
   constructor() { }
 
   ngOnChanges() {
     if (!this.media) return;
 
-    if (this.media.type === 'video') this.initVideo();
+    switch (this.media.type) {
+      case 'image':
+        this.image = <Image> this.media;
+        break;
+
+      case 'video':
+        this.video = <Video> this.media;
+        this.initVideo()
+        break;
+
+      default:
+        break;
+    }
   }
 
   private initVideo() {
     // Wait for video metadata to be ready
     let interval = setInterval(() => {
       // Metadata is available at readyState 1
-      if (!this.video || this.video.nativeElement.readyState === 0) return;
+      if (!this.video || this.templateVideo.nativeElement.readyState === 0) return;
 
       // Stopping interval
       clearInterval(interval);
 
       // Computing duration text
-      const d = this.video.nativeElement.duration;
+      const d = this.templateVideo.nativeElement.duration;
       const hours = Math.floor(d / 3600);
       const minutes = Math.floor(d / 60 - hours * 3600);
       const seconds = Math.floor(d - hours * 3600 - minutes * 60);
@@ -42,19 +58,19 @@ export class MediaComponent implements OnChanges {
         minutes + ':' +
         seconds;
 
-      this.video.nativeElement.playbackRate = 2;
+      this.templateVideo.nativeElement.playbackRate = 2;
 
       // Muting video
-      this.video.nativeElement.muted = 'muted';
-    });
+      this.templateVideo.nativeElement.muted = 'muted';
+    }, 10);
   }
 
   play() {
-    this.video.nativeElement.play();
+    this.templateVideo.nativeElement.play();
   }
 
   pause() {
-    this.video.nativeElement.pause();
+    this.templateVideo.nativeElement.pause();
   }
 
 }
